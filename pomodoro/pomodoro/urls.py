8c94c832cf714893bfaf5135483a8a635bd16126
vishvajitrao1998 <vishvajitrao1998@gmail.com>
2026-04-12 21:48:30 +0530

    changed project name

--- pomodoro/pomodoro/urls.py
+++ pomodoro/pomodoro/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for promodoro project.
+URL configuration for pomodoro project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("promotimer.urls")),
+    path("", include("pomotimer.urls")),
 ]
--- pomodoro/pomotimer/apps.py
+++ pomodoro/pomotimer/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PromotimerConfig(AppConfig):
+class PomotimerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "promotimer"
+    name = "pomotimer"
